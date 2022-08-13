6001afb6e2f96b4194811e4fb08f5ecb529eaf85
Nick Howes <nick@nickhowes.co.uk>
2022-08-13 12:04:45 +0100

    Pointless tweaks.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://nickhowes.co.uk',
+  integrations: [sitemap(), mdx()]
 });
